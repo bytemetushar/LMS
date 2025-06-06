fb0d6574aef759fe105ef168fb0d0fee837a3a31
bytemetushar <tusharvarshney620@gmail.com>
2025-06-06 14:59:51 +0530

    added login singup btn on sidebar

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer(){
     return (
         <>
             <footer className='relative left-0 bottom-0 h-[10vh] flex flex-col sm:flex-row items-center justify-between text-white bg-gray-800 sm:px-20'>
-                <section className='text-lg'>
+                <section className='text-lg font-semibold'>
                    Copyright {year} | All rights reserved.
                 </section>
                 <section className='flex items-center justify-center gap-5 text-2xl text-white'>
--- frontend/src/Layouts/HomeLayout.jsx
+++ frontend/src/Layouts/HomeLayout.jsx
@@ -1,9 +1,20 @@
 import {FiMenu} from 'react-icons/fi'
 import {AiFillCloseCircle} from 'react-icons/ai'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from '../Components/Footer';
+import { useDispatch, useSelector } from 'react-redux';
 
 function HomeLayout({children}){
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // for checking if user is ligged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    // for displaying the options according to role
+    const role = useSelector((state)=> state ?.auth?.role);
+
 
     function changeWidth(){
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -18,6 +29,13 @@ function HomeLayout({children}){
         drawerSide[0].style.width = '0';
     }
 
+    function handleLogOut(e){
+        e.preventDefault();
+
+        // const res = await dispatch(logout());
+        // if(res?.payload?.success)
+        // navigate("/"); 
+    }
 
     return(
         <div className="min-h-[90vh]">
@@ -44,6 +62,13 @@ function HomeLayout({children}){
                         <li>
                             <Link to="/">Home</Link>
                         </li>
+
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard">Admin DashBoard</Link>
+                            </li>
+                        )}
+
                         <li>
                             <Link to="/courses">All courses</Link>
                         </li>
@@ -53,6 +78,32 @@ function HomeLayout({children}){
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
+
+                        {!isLoggedIn && (
+                            <li className='absolute bottom-4 w-[90%]'>
+                                <div className='w-full flex items-center justify-center'>
+                                    <button className='bg-purple-800 px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/login">Login</Link>
+                                    </button>
+                                    <button className="bg-pink-600 px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link to="/signup">Singup</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
+                        
+                        {!isLoggedIn && (
+                            <li className='absolute bottom-4 w-[90%]'>
+                                <div className='w-full flex items-center justify-center'>
+                                    <button className='bg-purple-800 px-4 py-1 font-semibold rounded-md w-full'>
+                                        <Link to="/user/profile">Profile</Link>
+                                    </button>
+                                    <button className="bg-pink-600 px-4 py-1 font-semibold rounded-md w-full">
+                                        <Link onClick={handleLogOut}>Logout</Link>
+                                    </button>
+                                </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
