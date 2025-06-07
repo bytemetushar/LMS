78920274a5e1dc28654dc6716528b6132131ec2c
bytemetushar <tusharvarshney620@gmail.com>
2025-06-07 12:09:30 +0530

    pagenot found Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 import HomePage from './Pages/HomePage'
 import AboutUs from './Pages/AboutUs'
+import NotFound from './Pages/NotFound'
 
 
 
@@ -15,6 +16,8 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/about' element={<AboutUs/>}></Route>
+
+        <Route path='*' element={<NotFound/>}></Route>
       </Routes>
     </>
   )
