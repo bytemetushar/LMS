a9efc373557f33e699408487837ff76500858f5d
bytemetushar <tusharvarshney620@gmail.com>
2025-06-13 16:45:51 +0530

    course description added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './Pages/Login'
 import CourseList from './Pages/Course/CourseList'
 import Contact from './Pages/Contact'
 import Denied from './Pages/Denied'
+import CourseDescription from './Pages/Course/CourseDescription'
 
 
 
@@ -22,11 +23,14 @@ function App() {
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/about' element={<AboutUs/>}></Route>
         <Route path='/courses' element={<CourseList/>}></Route>
+        <Route path='/course/description' element={<CourseDescription/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/denied' element={<Denied/>}></Route>
+        
 
+
+        <Route path='/denied' element={<Denied/>}></Route>
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
     </>
